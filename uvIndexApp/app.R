bda884862088a82a41b69c87a0fe4399a0aaf831
naylynn-TR <ntanonreyes@smith.edu>
2021-03-26 16:57:45 -0700

    display plot in shiny web app

--- uvIndexApp/app.R
+++ uvIndexApp/app.R
@@ -8,12 +8,20 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(lubridate)
+library(rjson)
+library(jsonlite)
+library(RCurl)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     
     # Application title
     titlePanel("Old Faithful Geyser Data"),
+    
+    div(id = "header",
+        titlePanel("Explore your UV Index")),
     
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -27,7 +35,7 @@ ui <- fluidPage(
         
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("indexplot")
         )
     )
 )
@@ -42,6 +50,34 @@ server <- function(input, output) {
         
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    
+    output$indexplot <- renderPlot({
+        
+        uvrisk <- data.frame(name = c("Low", "Moderate", "High", "Very High"),
+                             imin = c(0,3,6,8),
+                             imax = c(3,6,8,11),
+                             mycolor = c("A", "B", "C", "D")) %>%
+            mutate(medy = imin + floor((imax-imin)/2))
+        
+        zipbase_url <- "https://enviro.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/"
+        zipfull_url <- paste0(zipbase_url, "95129/JSON")
+        uvdf <- fromJSON(readLines(zipfull_url))
+        
+        uvdf <- uvdf %>%
+            mutate(NEW_DATE = mdy_h(DATE_TIME))
+
+        ggplot() +
+            theme_light() +
+            geom_rect(data = uvrisk, aes(xmin = c(uvdf$NEW_DATE[1], uvdf$NEW_DATE[1], uvdf$NEW_DATE[1], uvdf$NEW_DATE[1]), xmax = c(uvdf$NEW_DATE[21], uvdf$NEW_DATE[21], uvdf$NEW_DATE[21], uvdf$NEW_DATE[21]), ymin = imin, ymax = imax, fill = mycolor)) +
+            geom_text(data = uvrisk, aes(x = uvdf$NEW_DATE[3], y = medy, label = name, size = 3))+
+            geom_line(data = uvdf, aes(x = NEW_DATE, y = UV_VALUE)) +
+            scale_y_continuous("UV Index", limit = c(-0.1, 11), breaks = c(0,1,2,3,4,5,6,7,8,9,10,11), expand = c(0, 0)) +
+            #scale_x_continuous(expand = c(0,0)) +
+            ggtitle("UV Index in the Past Day") +
+            theme(axis.title.x = element_blank(),
+                  legend.position = "None")
+        
     })
 }
 
