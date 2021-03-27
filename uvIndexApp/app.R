5e7e9794e7633c71aeba1352b3d74a84b7ca7aa1
naylynn-TR <ntanonreyes@smith.edu>
2021-03-26 21:23:45 -0700

    moved plot to the "uv index plot" tab

--- uvIndexApp/app.R
+++ uvIndexApp/app.R
@@ -13,75 +13,69 @@ library(lubridate)
 library(rjson)
 library(jsonlite)
 library(RCurl)
-library(shinythemes)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("simplex"),
+ui <- fluidPage(
     
-    shinyUI(navbarPage("UV Index App",
-                       tabPanel("UV Index"),
-                       tabPanel("Skin Protection"),
-                       tabPanel("Resources"),
-                       tabPanel("Credits")
+    shinyUI(navbarPage("UV App Home Page",
+                       tabPanel("Local UV Index Plot", 
+                                textInput("caption", "Enter your zipcode", "Zipcode"),
+                                # Application title
+                                div(id = "header",
+                                    titlePanel("Explore your local UV index")),
+                                plotOutput("zipplot")),
+                       tabPanel("Skin Protection",
+                                h4("Information")),
+                       tabPanel("Resources",
+                                h4("Information")),
+                       tabPanel("Credits",
+                                h4("Information"))
     )
     ),
     
-    # ask for zipcode
-    textInput("zipinput", "Enter your zipcode", "Zipcode"),
 
-    # Application title
-    div(id = "header",
-        titlePanel("Explore your local UV index")),
     
-    # Sidebar with a slider input for number of bins 
         
-        # plot of zip code
-        mainPanel(
-          plotOutput("zipplot")
-        )
+        # Show a plot of the generated distribution
+#        mainPanel(
+#          plotOutput("zipplot")
+#        )
 
-    )
-
+)
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    # ask for user input - zip code
-    output$value <- renderText({ input$zipinput })
+    output$value <- renderText({ input$caption })
     
-    zipcode <- reactive(input$zipinput)
+    zipcode <- reactive(input$caption)
     # zipcode <- reactive({
     #     validate(
     #         need(is.character(input$caption) != TRUE, "Please input a zipcode")
     #     )
     # })
-    
-    # data frame for uv classifications
-    uvrisk <- data.frame(name = c("Low", "Moderate", "High", "Very High"),
-                         imin = c(0,3,6,8),
-                         imax = c(3,6,8,11),
-                         mycolor = c("A", "B", "C", "D")) %>%
-        mutate(medy = imin + floor((imax-imin)/2))
-
 
     output$zipplot <- renderPlot({
         
+        uvrisk <- data.frame(name = c("Low", "Moderate", "High", "Very High"),
+                             imin = c(0,3,6,8),
+                             imax = c(3,6,8,11),
+                             mycolor = c("A", "B", "C", "D")) %>%
+            mutate(medy = imin + floor((imax-imin)/2))
+        
         zipbase_url <- "https://enviro.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/"
         zipfull_url <- paste0(zipbase_url, zipcode(), "/JSON")
-        zipdata <- as.data.frame(fromJSON(readLines(zipfull_url)))
+        uvdf <- as.data.frame(fromJSON(readLines(zipfull_url)))
         
-        zipdata <- zipdata %>%
-            mutate(NEW_DATE = mdy_h(DATE_TIME)) # get new time
-        
-        # plot zipdata
+        uvdf <- uvdf %>%
+            mutate(NEW_DATE = mdy_h(DATE_TIME))
+
         ggplot() +
             theme_light() +
-            geom_rect(data = uvrisk, aes(xmin = c(zipdata$NEW_DATE[1], zipdata$NEW_DATE[1], zipdata$NEW_DATE[1], zipdata$NEW_DATE[1]), 
-                                         xmax = c(zipdata$NEW_DATE[21], zipdata$NEW_DATE[21], zipdata$NEW_DATE[21], zipdata$NEW_DATE[21]), 
-                                         ymin = imin, ymax = imax, fill = mycolor)) +
-            geom_text(data = uvrisk, aes(x = zipdata$NEW_DATE[3], y = medy, label = name, size = 3))+
-            geom_line(data = zipdata, aes(x = NEW_DATE, y = UV_VALUE)) +
+            geom_rect(data = uvrisk, aes(xmin = c(uvdf$NEW_DATE[1], uvdf$NEW_DATE[1], uvdf$NEW_DATE[1], uvdf$NEW_DATE[1]), xmax = c(uvdf$NEW_DATE[21], uvdf$NEW_DATE[21], uvdf$NEW_DATE[21], uvdf$NEW_DATE[21]), ymin = imin, ymax = imax, fill = mycolor)) +
+            geom_text(data = uvrisk, aes(x = uvdf$NEW_DATE[3], y = medy, label = name, size = 3))+
+            geom_line(data = uvdf, aes(x = NEW_DATE, y = UV_VALUE)) +
             scale_y_continuous("UV Index", limit = c(-0.1, 11), breaks = c(0,1,2,3,4,5,6,7,8,9,10,11), expand = c(0, 0)) +
             #scale_x_continuous(expand = c(0,0)) +
             ggtitle("UV Index in the Past Day") +
@@ -89,7 +83,6 @@ server <- function(input, output) {
                   legend.position = "None")
         
     })
-    
 }
 
 # Run the application 
